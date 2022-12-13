63575c3170164f0587d834d783fdf01a91a92b17
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-12-13 12:53:47 +0300

    Add files via upload

---
+++ middle_str.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include "middle_str.h"
+using namespace std;
+long long itc_len(string str){
+     int kol,i;
+     kol=0;
+     i=0;
+     while(str [i]!= '\0'){
+      kol++;
+      i++;
+ }
+ return kol;
+ }
+ bool itc_isDigit(unsigned char c){
+     int a;
+     if(c >= 48 && c <= 57)
+        return 1;
+     else
+        return 0;
+ }
+unsigned char itc_toUpper(unsigned char c){
+    if(c>=97 && c<=122)
+        return c-32;
+    else
+        return c;
+}
+unsigned char itc_changeCase(unsigned char c){
+    if(c>=97 && c<=122)
+        return c-32;
+     if(c>=65 && c<=90)
+        return c+32;
+    else
+        return c;
+
+}
+bool itc_compare(string s1, string s2) {
+    long long lenght1 = itc_len(s1);
+    long long lenght2 = itc_len(s2);
+    int a;
+    for (int i = 0; i < lenght2 || i<lenght1; i++){
+        if (s1[i] != s2[i])
+            a++;
+}
+    if(a>0)
+            return 0;
+    return 1;
+}
+int itc_countWords(string str){
+long long lenght = itc_len(str);
+int amount = 0;
+        for(int i=0;i<lenght;i++){
+                if(str[i] == ' ' ) {
+                amount++;
+            }
+        }
+        return amount;
+}
---
+++ middle_str.h
@@ -0,0 +1,14 @@
+#ifndef MIDDLE_STR_H_INCLUDED
+#define MIDDLE_STR_H_INCLUDED
+#include <iostream>
+#include <string>
+#include "middle_str.h"
+using namespace std;
+long long itc_len(string str);
+bool itc_isDigit(unsigned char c);
+unsigned char itc_toUpper(unsigned char c);
+unsigned char itc_changeCase(unsigned char c);
+bool itc_compare(string s1, string s2);
+int itc_countWords(string str);
+
+#endif // MIDDLE_STR_H_INCLUDED
