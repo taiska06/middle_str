d1c6bcff2322113c2b4cd0beba9e883507ddbc4f
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-12-26 22:29:00 +0300

    Update middle_str.cpp

--- middle_str.cpp
+++ middle_str.cpp
@@ -2,16 +2,6 @@
 #include <string>
 #include "middle_str.h"
 using namespace std;
-long long itc_len(string str){
-     int kol,i;
-     kol=0;
-     i=0;
-     while(str [i]!= '\0'){
-      kol++;
-      i++;
- }
- return kol;
- }
 string itc_Cezar(string str, int k){
 long long length = itc_len(str);
 string str1;
