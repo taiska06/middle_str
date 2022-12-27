c9e8331bd6edb74a6732ccd8a8b4f866f654603a
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-12-27 11:47:21 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -15,4 +15,5 @@ char itc_sameChar(string str);
 bool itc_isFirstInSecond(string s1, string s2);
 bool itc_isIp(string str);
 string itc_rmFreeSpace(string str);
+string itc_Cezar(string str, int k);
 #endif // MIDDLE_STR_H_INCLUDED
